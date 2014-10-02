62fb513fe875ead3e0ac4a0f650d2f9c329ec629
Parag Dadhaniya <paragdadhaniya@gmail.com>
2014-10-02 09:21:04 -0700

    add table with game history

--- web/public/rps.js
+++ web/public/rps.js
@@ -1,3 +1,11 @@
 $('.addPlayer').click(function() {
   $('.playerForm').css('display', 'block');
 });
+
+$('.gamesTable').click(function() {
+  $('.gameTable').css('display', 'block');
+});
+
+$('.hideTable').click(function() {
+  $('.gameTable').css('display', 'none');
+})
